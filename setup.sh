9433d70ff5f7b3ac94c17ce9bcb4630b8433b321
Ranjhith Kalisamy <ranjhith.kumar@gmail.com>
2011-10-07 18:12:21 +0530

    Initial commit

---
+++ setup.sh
@@ -0,0 +1,22 @@
+# Shell script to setup symlinks in $HOME to the dotfiles here
+# This script is meant to be run on new machines with newly created user
+# account.
+
+function symlink {
+    target=$1
+    link=$2
+    echo "symlink: $link => $target"
+    rm $link
+    ln -sf $target $link
+}
+
+for dotfile in .emacs .bashrc-custom.sh .fonts .gitconfig .hgrc .screenrc \
+               .xbindkeysrc .zshrc .vimrc .buildout .sources .tmux.conf;
+do
+symlink `pwd`/$dotfile ~/$dotfile;
+done
+
+echo "Read this script to see programs to install."
+# apt-get install emacs-snapshot-gtk tmux zsh git-core wmctrl \
+# build-essential python-dev ack-grep colordiff multitail tig \
+# msttcorefonts
